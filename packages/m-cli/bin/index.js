8114d6e50ecffe907c5fa5052b62be2ca9bfe2fc
wuhaoming1 <wuhaoming1@tal.com>
2024-07-25 11:35:36 +0800

    feat: init

---
+++ packages/m-cli/bin/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+const path = require('path');
+const { inquirerPrompt } = require("./inquirer");
+const { copyDir, checkMkdirExists } = require("./copy");
+
+yargs.command(
+  ['create', 'c'],
+  '新建一个模板',
+  function (yargs) {
+    return yargs.option('name', {
+      alias: 'n',
+      demand: true,
+      describe: '模板名称',
+      type: 'string'
+    })
+  },
+  function (argv) {
+    inquirerPrompt(argv).then(answers => {
+      const { name, type } = answers;
+      const isMkdirExists = checkMkdirExists(
+        path.resolve(process.cwd(),`./src/pages/${name}`)
+      );
+      if (isMkdirExists) {
+        console.log(`${name}文件夹已经存在`)
+      } else {
+        copyDir(
+          path.resolve(__dirname, `../../template/${type}.js`),
+          path.resolve(process.cwd(), `./src/pages/${name}`)
+        )
+      }
+    })
+  }
+).argv;
---
+++ packages/m-cli/index.js
@@ -0,0 +1,5 @@
+const process = require('process');
+// 获取命令参数
+console.log(process.argv); 
+
+console.log('hello from m-cli');
